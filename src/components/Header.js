aecd889c94994d6136401954209baf6bd33fbd06
Abhi Diddigi <adiddigi@apple.com>
2020-03-21 00:07:05 -0700

    making changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,8 @@ const Header = props => (
       <div className="inner">
         <h1>Ashwini Munji</h1>
         <p>
-          An Interior Designer currently working in the San Fransisco Bay Area. She has Bachelors in Interior Design from JNTU University, Hyderabad, India. Her research
-          interests include Landcape Architecture and Architecture. 
+          Hi, I'm an Interior designer based out of San Fransisco Bay Area. I've a Bachelors of Interior Design from JNAFAU, Hyderabad. Currently open to opportunities including freelance projects. I think of myself as a versatile designer with various interests including but not
+          limited to Architecture and Landscape Architecture. 
         </p>
       </div>
     </div>
