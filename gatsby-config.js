1e96b7001bdc791e1e592623c0f69af6c3d9f4fd
Abhi Diddigi <adiddigi@apple.com>
2020-03-20 23:34:25 -0700

    making changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Ashwini Munji',
+    author: 'Hunter Chang, Ashwini Munji',
+    description: 'Ashwini Munji Landing page. ',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
