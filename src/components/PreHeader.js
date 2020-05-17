3406a3b1d8570a969b168d1137b19b3f3c89d449
Abhi Diddigi <adiddigi@apple.com>
2020-05-17 13:58:29 -0700

    more changes

--- src/components/PreHeader.js
+++ src/components/PreHeader.js
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const PreHeader = props => (
-  //<div class="fas fa-moon" style={{position: 'absolute;top: 10px;right: 10px'}} onClick={() => {props.handleBGChange()}}>Change Color </div>
-  <span className="icon fa-moon-o" style={{position: 'absolute;top: 10px;right: 10px', display: '{props.getDisplayProp('moon')}'}} onClick={() => {props.handleBGChange()}}></span>
-  <span className="icon fa-sun-o" style={{position: 'absolute;top: 10px;right: 10px', display: '{props.getDisplayProp('sun')}' }} onClick={() => {props.handleBGChange()}}></span>
+  // <span className="icon fa-sun-o" style={{position: 'absolute;top: 10px;right: 10px', display: '{props.getDisplayProp('sun')}' }} onClick={() => {props.handleBGChange()}}></span>
+  <span className="icon fa-moon-o" style={{position: 'absolute;top: 10px;right: 10px'}} onClick={() => {props.handleBGChange()}}></span>
+ 
 )
 
 PreHeader.propTypes = {
