e4a21d14bd67a8190f209605ac771f101e836f34
Abhi Diddigi <adiddigi@apple.com>
2020-05-17 13:23:44 -0700

    more hardcoding

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from '../components/layout'
 import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
+import PreHeader from '../components/PreHeader'
 
 class IndexPage extends React.Component {
   constructor(props) {
