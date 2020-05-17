569929eb4c5ce7bf49111689655cfb456d3cb3ec
Abhi Diddigi <adiddigi@apple.com>
2020-05-17 14:18:41 -0700

    more changes

--- src/components/PreHeader.js
+++ src/components/PreHeader.js
@@ -13,7 +13,6 @@ var sunStyle = {
 
 const PreHeader = props => (
   <div>
-  // <span className="icon fa-sun-o" style={{position: 'absolute;top: 10px;right: 10px', display: '{props.getDisplayProp('sun')}' }} onClick={() => {props.handleBGChange()}}></span>
   <span id="moon" className="icon fa-moon-o" style={moonStyle} onClick={() => {props.handleBGChange()}}></span>
   <span id="sun" className="icon fa-sun-o" style={sunStyle} onClick={() => {props.handleBGChange()}}></span>
   </div>
