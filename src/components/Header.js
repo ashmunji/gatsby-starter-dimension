554e773b69c47dca2fc8ebaadfd97c439db3c0a0
ashmunji <51296685+ashmunji@users.noreply.github.com>
2020-06-14 00:01:38 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,18 +9,19 @@ const Header = props => (
         <h1 id="headerName">Ashwini Munji</h1>
         <p>
           Hi, I'm the name you see above or simply Nimu. I'm currently in graduate school pursuing a Master's in Landscape Architecture. I was born in  Hyderabad, India, currently residing in the Bay Area.
-
-I earned my Bachelor's Degree in Interior Design from JNAFAU. After working for a couple of small and large-scale firms, I quit my full-time job to pursue my other interests and to travel. 
-During this time, I rekindled my love for the arts. I'm a self-taught illustrator and an amateur animator. You can find my works on Instagram, Dribbble, and Behance. To see the process behind some of my artwork, please visit my Youtube channel. Don't forget to check my Gumroad account for some free giveaways.
-
-During my spare time, I like to experiment with cooking, learn a new craft or blog.
-
-When the lockdown was declared, I wrote and sketched my first illustrated series - The adventures of Nimu, Roro & Munni.
-
-I also like to believe that I'm a problem solver and constantly think of innovative ways to do mundane tasks. (More on this soon) 
         </p>
         <p>
-        I'm a versatile designer with interests including but not limited to Art, Architecture and Cooking.
+        I earned my Bachelor's Degree in Interior Design from JNAFAU. After working for a couple of small and large-scale firms, I quit my full-time job to pursue my other interests and to travel. 
+        During this time, I rekindled my love for the arts. I'm a self-taught illustrator and an amateur animator. You can find my works on Instagram, Dribbble, and Behance. To see the process behind some of my artwork, please visit my Youtube channel. Don't forget to check my Gumroad account for some free giveaways.
+        </p>
+        <p>
+        During my spare time, I like to experiment with cooking, learn a new craft or blog.
+        </p>
+        <p>
+        When the lockdown was declared, I wrote and sketched my first illustrated series - The adventures of Nimu, Roro & Munni.
+        </p>
+        <p>
+        I also like to believe that I'm a problem solver and constantly think of innovative ways to do mundane tasks. (More on this soon) 
         </p>
       </div>
     </div>
